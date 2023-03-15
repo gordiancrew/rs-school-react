1ed972eb751558ae2221f9835403472ee1b2a255
Uladzimir Kazantsau <gordiancrew@gmail.com>
2023-03-16 00:49:50 +0300

    feat: add routing

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,21 +1,29 @@
-import { useState } from 'react';
 import './App.css';
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Main from './components/start';
+import AboutUs from './components/about-us';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <div></div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+    // <div className="App">
+    //   <div></div>
+    //   <h1>Vite + React</h1>
+    //   <div className="card">
+    //     <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+    //     <p>
+    //       Edit <code>src/App.tsx</code> and save to test HMR
+    //     </p>
+    //   </div>
+    //   <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+    // </div>
+    <div>
+      <div>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/about" element={<AboutUs />} />
+        </Routes>
       </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </div>
   );
 }
---
+++ vite-project/src/components/about-us.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+class AboutUs extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>This is class ABOUT US</h1>
+      </div>
+    );
+  }
+}
+
+export default AboutUs;
