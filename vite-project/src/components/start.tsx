3e5b8c7001c04d35e27a0101d3428930a757bb3c
Uladzimir Kazantsau <gordiancrew@gmail.com>
2023-03-16 19:53:18 +0300

    feat: add search

--- vite-project/src/components/start.tsx
+++ vite-project/src/components/start.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Search from './utils/search';
 
 class Main extends React.Component {
   render(): React.ReactNode {
     return (
       <div>
+        <Search />
         <h1>Class main</h1>
       </div>
     );
---
+++ vite-project/src/components/utils/search.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../../styles/search.css';
+class Search extends React.Component {
+  render() {
+    return (
+      <div className="d1">
+        <form>
+          <input />
+          <button type="submit"></button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Search;
