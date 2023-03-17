612b525784928f61f8273d959b136f2d69dda1bd
Uladzimir Kazantsau <gordiancrew@gmail.com>
2023-03-18 00:24:58 +0300

    feat: deploy

--- vite-project/src/components/about-us.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-class AboutUs extends React.Component {
-    render() {
-        return (React.createElement("div", null,
-            React.createElement("h1", null, "This is class ABOUT US")));
-    }
-}
-export default AboutUs;
--- vite-project/src/components/not-found.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-class NotFound extends React.Component {
-    render() {
-        return (React.createElement("div", null,
-            React.createElement("h1", null, "404")));
-    }
-}
-export default NotFound;
